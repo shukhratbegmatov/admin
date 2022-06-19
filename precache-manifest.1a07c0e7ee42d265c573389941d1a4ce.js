759d9f64d7205579046314417920e2a7d94df457
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-06-19 16:51:32 +0500

    change api

---
+++ precache-manifest.1a07c0e7ee42d265c573389941d1a4ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d651b48a487ddb95649f02f1b2b8797",
+    "url": "/Json/districts.json"
+  },
+  {
+    "revision": "51475e1f3c1e269831fce4cf7e663670",
+    "url": "/Json/kvartals.json"
+  },
+  {
+    "revision": "0e9c7393050d0ffabfb88d9523040574",
+    "url": "/Json/regions.json"
+  },
+  {
+    "revision": "31d9201096ab24dc203a",
+    "url": "/css/about.7e136814.css"
+  },
+  {
+    "revision": "75a132dd46308c7f3cfb",
+    "url": "/css/app.9aee812c.css"
+  },
+  {
+    "revision": "394613e9a1f50660ffe5",
+    "url": "/css/chunk-vendors.86dd1494.css"
+  },
+  {
+    "revision": "c94832e62f702c47cd697992ec514acf",
+    "url": "/img/sport.c94832e6.jpg"
+  },
+  {
+    "revision": "9b6a8b3281f37e8f157993b355c68b54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31d9201096ab24dc203a",
+    "url": "/js/about.9983975f.js"
+  },
+  {
+    "revision": "75a132dd46308c7f3cfb",
+    "url": "/js/app.193f9129.js"
+  },
+  {
+    "revision": "394613e9a1f50660ffe5",
+    "url": "/js/chunk-vendors.52c7e1e5.js"
+  },
+  {
+    "revision": "5cc7ae5e52993f324f4b5e6c5eca1b3b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
